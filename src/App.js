cb4cd1ac6cd5fa891a67c03fd08df1f11ae08b4f
ppsreejith <mailz4sreejith@gmail.com>
2018-04-01 18:05:06 +0530

    Centered map

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ class App extends Component {
         <div className='App-header'>
           <h2>Karnataka elections</h2>
         </div>
-        <div>
+        <div style={{flex: 1, justifyContent: 'center', flexDirection: 'row', display: 'inline-block'}}>
+          <Karnataka data={[5,10,1,3]} size={[500,500]} />
           <Karnataka data={[5,10,1,3]} size={[500,500]} />
         </div>
       </div>
--- src/components/Karnataka/index.js
+++ src/components/Karnataka/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Dimensions from 'react-dimensions'
 
 import { geoPath, geoMercator } from 'd3-geo';
 import { scaleLinear } from 'd3-scale';
@@ -19,16 +20,17 @@ class Karnataka extends Component {
   componentDidUpdate() {
     this.createBarChart()
   }
+  
   createBarChart() {
     const node = this.node
     const [width, height] = this.props.size;
 
-    const path = geoPath().projection(geoMercator().scale(2200));
+    const path = geoPath().projection(geoMercator().scale(3000));
     
     select(node)
       .append("g")
       .attr("class", "states")
-      .attr("transform","translate(-3200,700)")
+      .attr("transform","translate(-4200,750)")
       .selectAll("path")
       .data(topojson.feature(geoJSONData, geoJSONData.objects.karnataka).features)
       .enter().append("path").attr("class","constituencies")
@@ -38,11 +40,17 @@ class Karnataka extends Component {
       })
       .attr("d", path);
   }
+
   render() {
     return (
-      <svg ref={node => this.node = node}
-           width={500} height={500}>
-      </svg>
+      <div style={{display: 'inline-block', flex: 1}}>
+        <svg ref={node => this.node = node}
+             width={this.props.size[0]}
+             style={{border: '1px black solid'}}
+             height={this.props.size[0]}
+        >
+        </svg>
+      </div>
     )
   }
 }
