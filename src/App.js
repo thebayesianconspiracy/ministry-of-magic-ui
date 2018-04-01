65c176971e69b6ed95b2e9ed75adb91e5cbf91ca
ppsreejith <mailz4sreejith@gmail.com>
2018-04-01 16:43:50 +0530

    Working version

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Component } from 'react'
+import './App.css'
+import BarChart from './components/BarChart'
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className='App'>
+        <div className='App-header'>
+          <h2>Karnataka elections</h2>
+        </div>
+        <div>
+          <BarChart data={[5,10,1,3]} size={[500,500]} />
+        </div>
       </div>
-    );
+    )
   }
 }
-
-export default App;
+export default App
